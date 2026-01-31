e018753309c8007e6e8be911d0cfbe6725776e65
sweetpiedmont <sharon.hays@sweetpiedmont.com>
2026-01-31 12:57:23 -0500

    still debugging

--- BB_pricing_mvp.py
+++ BB_pricing_mvp.py
@@ -94,7 +94,7 @@ pricing_df = load_master_pricing(
     "/Users/sharon/Library/CloudStorage/OneDrive-Personal/Bouquet Recipes/CANONICAL Bouquet Recipe Master Sheet.xlsx"
 )
 
-st.write(pricing_df.head())
+st.dataframe(pricing_df[["season_raw", "category", "wholesale_price"]])
 
 def calculate_stem_recipe(total_stems, recipe_percentages):
     """
