6cc2a951368d0ad355040e15cc9d33e7c273b92b
sweetpiedmont <sharon.hays@sweetpiedmont.com>
2026-01-31 17:06:50 -0500

    same as last

--- BB_pricing_mvp.py
+++ BB_pricing_mvp.py
@@ -11,8 +11,8 @@ CANONICAL_RECIPES = {
         "Foliage": 0.15,
     },
     "late_spring": {
-        "Focal": 0.15,
-        "Foundation": 0.40,
+        "Focal": 0.12,
+        "Foundation": 0.43,
         "Filler": 0.10,
         "Floater": 0.10,
         "Finisher": 0.10, 
