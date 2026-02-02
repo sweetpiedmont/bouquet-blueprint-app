b2b8bcf88f41929abf94f938951f2397d8e6495a
sweetpiedmont <sharon.hays@sweetpiedmont.com>
2026-02-02 15:42:04 -0500

    created canonical recipes file and put recipe code in it

---
+++ core/canonical_recipes.py
@@ -0,0 +1,32 @@
+CANONICAL_RECIPES = {
+    "early_spring": {
+        "Focal": 0.20,
+        "Foundation": 0.39,
+        "Filler": 0.08,
+        "Floater": 0.10,
+        "Finisher": 0.08,
+        "Foliage": 0.15,
+    },
+    "late_spring": {
+        "Focal": 0.12,
+        "Foundation": 0.43,
+        "Filler": 0.10,
+        "Floater": 0.10,
+        "Finisher": 0.10,
+        "Foliage": 0.15,
+    },
+    "summer_fall": {
+        "Focal": 0.33,
+        "Foundation": 0.20,
+        "Filler": 0.10,
+        "Floater": 0.11,
+        "Finisher": 0.11,
+        "Foliage": 0.15,
+    },
+}
+
+SEASON_KEY_TO_RECIPE_SEASON = {
+    "early_spring": "Early Spring",
+    "late_spring": "Late Spring",
+    "summer_fall": "Summer/Fall",
+}
